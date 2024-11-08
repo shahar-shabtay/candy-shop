3ec42331e55bcc018896f572ae01678e9248e468
zivklein21 <zivklein@Zivs-MacBook-Air.local>
2024-11-08 22:29:26 +0200

    add hostages countdown to all pages

--- public/js/main.js
+++ public/js/main.js
@@ -135,3 +135,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+(function () {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://bringthemhomenow.net/1.1.0/hostages-ticker.js";
+    script.setAttribute(
+      "integrity",
+      "sha384-DHuakkmS4DXvIW79Ttuqjvl95NepBRwfVGx6bmqBJVVwqsosq8hROrydHItKdsne"
+    );
+    script.setAttribute("crossorigin", "anonymous");
+    document.getElementsByTagName("head")[0].appendChild(script);
+  })();

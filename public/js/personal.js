e08f95a041be3b167b62fe653bd23653e410871b
zivklein21 <zivklein@Zivs-MacBook-Air.local>
2024-11-08 23:22:21 +0200

    fix order section

--- public/js/personal.js
+++ public/js/personal.js
@@ -238,8 +238,8 @@ async function removeFavorite(productId) {
 //--------------
 
 // See order details - work
-async function showCustOrder() {
-    const orderId = document.getElementById('see-order').getAttribute('data-order-id');
+async function showCustOrder(element) {
+    const orderId = element.getAttribute('data-order-id');
     window.location.href = `/personal/myAccount/orders/${orderId}`;
 }
 
@@ -471,8 +471,8 @@ document.querySelectorAll('.remove').forEach(icon => {
 });
 
 // See order details - works
-async function seeOrder() {
-    const orderId = document.getElementById('see-order').getAttribute('data-order-id');
+async function seeOrder(element) {
+    const orderId = element.getAttribute('data-order-id');
     window.location.href=`/personal/admin/orders/${orderId}`;
 }
 
